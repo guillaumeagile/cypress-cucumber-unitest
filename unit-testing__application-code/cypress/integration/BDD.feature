0c75b440a6c4cb36c6fe65ab45cb6df429118b80
g.saintetienne <g.saintetienne@primobox.com>
2021-05-05 17:47:32 +0200

    mes modifs après le cours

--- unit-testing__application-code/cypress/integration/BDD.feature
+++ unit-testing__application-code/cypress/integration/BDD.feature
@@ -1,13 +1,19 @@
 Feature: FizzBuzz BDD
-
   I want to start a FizzBuzz game
 
-  Scenario: the number 3 should fizz
-    Given I try with 3    
-    Then the output shall be fizz
+Scenario: the number 3 should fizz
+    Given I try with 3
+    When j'appelle la fonction
+    Then the output shall be 'fizz'
 
 Scenario: the number 5 should buzz
     Given I try with 5
-    Then the output shall be: 'buzz'
+    When j'appelle la fonction
+    Then the output shall be 'buzz' and not 'fizz'
+    And the number was 5
 
-
+    Scenario: the number 15 should fizzbuzz
+    Given I try with 15
+    When j'appelle la fonction
+    Then the output shall be 'fizzbuzz' and not 'fizz' 
+    And the number was 15
--- unit-testing__application-code/cypress/integration/common/steps.js
+++ unit-testing__application-code/cypress/integration/common/steps.js
@@ -1,20 +1,40 @@
 /// <reference types="cypress" />
-import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
-import fizzbuzz from '../../../fizzbuzz';
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+import fizzbuzz from '../../../fizzbuzz'
 
-let inputNumber = 0;
+let inputNumber = 0
+let resultat = ''
 
-Given(`I try with {int}`, (i) => {
-  inputNumber = i
-});
+Given(`I try with {int}`, (num) => {
+  inputNumber = num
+})
 
+When(`j'appelle la fonction`, () => {
+  resultat = fizzbuzz(inputNumber)
+})
 
-Then(`the output shall be: {string}`, (text) => {
-  expect(fizzbuzz(inputNumber)).to.eq(text)
-});
-
+Then(`the output shall be: {string} and not {string}`, (text) => {
+  expect(resultat).to.eq(text)
+})
 
 //  with regular expression to match a string
-Then(/^the output shall be ([^"]*)$/, (text) => {
+Then(/^the output shall be \'([^']*)\'$/, (text) => {
+  expect(fizzbuzz(inputNumber)).to.eq(text)
+})
+
+//  with regular expression to match a string
+Then(/^the output shall be \'([^']*)\' and not \'([^']*)\'$/, (text1, text2) => {
+  expect(fizzbuzz(inputNumber)).to.eq(text1)
+  expect(fizzbuzz(inputNumber)).to.not.eq(text2)
+})
+
+Then(/^the number was (-?\d+)$/, (num) => {
+  expect(num).to.equal(inputNumber)
+})
+
+/*
+//  with regular expression to match a string
+Then(/^the output shall be (.+)$/, (text) => {
   expect(fizzbuzz(inputNumber)).to.eq(text);
 });
+*/
